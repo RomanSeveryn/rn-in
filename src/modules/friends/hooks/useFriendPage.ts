e13fbf6ed88c667462d9b00d8ddc1e7a32802fcf
Roman Severyn <romaseverin50@gmail.com>
2022-07-05 16:31:44 +0300

    create hook for Friend Page

---
+++ src/modules/friends/hooks/useFriendPage.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { getUsers } from '../../../api/basic';
+import get from 'lodash/get';
+import { FriendType, QueryFriendKey } from '../types';
+
+export const useFriendPage = () => {
+  const [users, setUsers] = useState<Array<FriendType>>([]);
+  const [text, onChangeText] = useState<string>('');
+
+  useQuery(QueryFriendKey.FRIEND, () => getUsers, {
+    onSuccess: (response) => {
+      const userArray = get(response, ['data'], []);
+      setUsers(userArray);
+    },
+  });
+
+  const filteredUsers: Array<FriendType> = users.filter((user) =>
+    user.name.toLowerCase().includes(text.toLowerCase()),
+  );
+
+  return { filteredUsers, text, onChangeText };
+};
