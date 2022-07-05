7d5ccdafe563ba76f1e2477188fea657c42434c0
Roman Severyn <romaseverin50@gmail.com>
2022-07-05 16:34:13 +0300

    create Friend context

--- src/app/navigator/AppNavigator.tsx
+++ src/app/navigator/AppNavigator.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { InnerScreens } from './components/InnerScreens/InnerScreens';
+import { FriendProvider } from '../../providers/friend/FriendProvider';
 
 export const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <InnerScreens />
+      <FriendProvider>
+        <InnerScreens />
+      </FriendProvider>
     </NavigationContainer>
   );
 };
