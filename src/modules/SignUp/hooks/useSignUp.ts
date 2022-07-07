64db61c2e6270cab0b9cc3acb6351ee79043e957
Roman Severyn <romaseverin50@gmail.com>
2022-07-07 13:16:04 +0300

    create hook for sign up page

---
+++ src/modules/SignUp/hooks/useSignUp.ts
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { UserType } from './types';
+
+const initialValues = {
+  name: '',
+  lastName: '',
+  email: '',
+  password: '',
+};
+
+export const useSignUp = () => {
+  const [currentUser, setCurrentUser] = useState<UserType>(initialValues);
+
+  const saveUser = (values: UserType) => {
+    setCurrentUser((prev) => ({
+      ...prev,
+      values,
+    }));
+  };
+
+  const isLoggedIn: boolean = !!currentUser.name;
+
+  return {
+    currentUser,
+    initialValues,
+    isLoggedIn,
+    saveUser,
+  };
+};
